801fce1dcac2e370854c8f3fe2864f233562b237
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-10-06 18:38:09 +0700

    update font

---
+++ app/src/main/java/com/nguyenminhkhang/travelui/home/components/CategoryTabs.kt
@@ -0,0 +1,54 @@
+package com.nguyenminhkhang.travelui.home.components
+
+import android.view.RoundedCorner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import com.nguyenminhkhang.travelui.ui.theme.RadiusXS
+import com.nguyenminhkhang.travelui.ui.theme.SpaceM
+import com.nguyenminhkhang.travelui.R
+
+data class CategoryUiState(
+    val title: String,
+    val isSelected: Boolean
+)
+
+@Composable
+fun CategoryTabs(modifier : Modifier, uiState: CategoryUiState) {
+    Box(
+        modifier = modifier.background(Color.Black, shape = RoundedCornerShape(SpaceM)).padding(SpaceM)
+    ) {
+        Text(
+            text = uiState.title,
+            color = Color.White,
+            style = MaterialTheme.typography.labelLarge.copy(
+                fontFamily = FontFamily(Font(R.font.roboto_medium))
+            ),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CategoryTabsPreview() {
+    CategoryTabs(
+        modifier = Modifier,
+        uiState = CategoryUiState(
+            title = "Most Viewed",
+            isSelected = true
+        )
+    )
+}
