398f574e7c97ae9ee27c6fb6a431e631155852b0
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-10-06 17:59:15 +0700

    init project

---
+++ app/src/main/java/com/nguyenminhkhang/travelui/ui/theme/Dimes.kt
@@ -0,0 +1,34 @@
+package com.nguyenminhkhang.travelui.ui.theme
+
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+// Radius values
+val RadiusXS = 2.dp
+val RadiusSmall = 4.dp
+val RadiusMedium = 6.dp
+val RadiusLarge = 15.dp
+val RadiusXL = 20.dp
+val RadiusXXL = 25.dp
+val RadiusXXXL = 30.dp
+
+// Spacing values
+val SpaceXXXXS = 1.dp
+val SpaceXXXS = 2.dp
+val SpaceXXS = 4.dp
+val SpaceXS = 8.dp
+val SpaceS = 12.dp
+val SpaceM = 16.dp
+val SpaceL = 24.dp
+val SpaceXL = 32.dp
+val SpaceXXL = 40.dp
+val SpaceXXXL = 48.dp
+val SpaceXXXXL = 56.dp
+
+// Icon sizes
+val IconSizeXXS = 12.dp
+val IconSizeXS = 16.dp
+val IconSizeS = 22.dp
+val IconSizeM = 23.dp
+val IconSizeL = 24.dp
+val IconSizeXL = 48.dp
